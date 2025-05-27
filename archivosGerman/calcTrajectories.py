f6a05d385152a189f67d594ff486fbbb62d547c9
mauritobaez <mauroleanbaez@gmail.com>
2025-05-27 19:12:11 +0200

    Mejora significativa al all y al cálculo de trayectorias

--- archivosGerman/calcTrajectories.py
+++ archivosGerman/calcTrajectories.py
@@ -31,9 +31,70 @@ def hampel_filter(v, window_size=7, n_sigmas=3):
             filtered[i] = med  # reemplaza outlier por mediana local
     return filtered
 
+def moving_average_smoothing(v, window_size=5):
+    """
+    v: array 1D de velocidades (o posiciones)
+    window_size: longitud de la ventana (debe ser impar)
+    """
+    if window_size % 2 == 0:
+        raise ValueError("window_size debe ser impar")
+    
+    half_window = window_size // 2
+    smoothed = np.zeros_like(v)
+    
+    for i in range(len(v)):
+        lo = max(i - half_window, 0)
+        hi = min(i + half_window + 1, len(v))
+        smoothed[i] = np.mean(v[lo:hi])
+    
+    return smoothed
+
+def finite_difference_left(x, y, dt):
+    """
+    Calcula las velocidades usando diferencia finita por izquierda
+    
+    x: array 1D de posiciones en el eje x
+    y: array 1D de posiciones en el eje y
+    dt: intervalo de tiempo entre mediciones
+    """
+    vx = np.zeros(len(x))
+    vy = np.zeros(len(y))
+    
+    for i in range(1, len(x)):
+        vx[i] = (x[i] - x[i-1]) / dt
+        vy[i] = (y[i] - y[i-1]) / dt
+    
+    # Manejo de los extremos
+    vx[0] = (x[1] - x[0]) / dt
+    vy[0] = (y[1] - y[0]) / dt
+    
+    return vx, vy
+
+def finite_difference_centered(x, y, dt):
+    """
+    Calcula las velocidades usando diferencia finita por izquierda
+    
+    x: array 1D de posiciones en el eje x
+    y: array 1D de posiciones en el eje y
+    dt: intervalo de tiempo entre mediciones
+    """
+    vx = np.zeros(len(x))
+    vy = np.zeros(len(y))
+    
+    for i in range(1, len(x)-1):
+        vx[i] = (x[i+1] - x[i-1]) / (2*dt)
+        vy[i] = (y[i+1] - y[i-1]) / (2*dt)
+    
+    # Manejo de los extremos
+    vx[0] = (x[1] - x[0]) / dt
+    vy[0] = (y[1] - y[0]) / dt
+    vx[-1] = (x[-1] - x[-2]) / dt
+    vy[-1] = (y[-1] - y[-2]) / dt
+    
+    return vx, vy
 
 
-def velocities(x,y,dt):
+def five_point_stencil(x,y,dt):
     vx = np.zeros(len(x))
     vy = np.zeros(len(y))
     
@@ -56,7 +117,7 @@ def velocities(x,y,dt):
     return vx,vy
 
 inputFolder = './archivosGerman/pedestrianTrajectories/'
-outputFolder = './archivosGerman/pedestrianTrajectoriesProcessedSmoothAndFilter/'
+outputFolder = './archivosGerman/stencilMovingAverageHampel/'
 import os
 os.makedirs(outputFolder, exist_ok=True)
 
@@ -95,19 +156,21 @@ for i in range(len(inputFile)):
     Y = (-(yB-yA)*x+(xB-xA)*y)/r/r*DIST_AB
 
     # SMOOTHING DE LAS POSICIONES
-    X = savgol_filter(X, window_length=7, polyorder=3) # Hasta ahora: 7 y 3
-    Y = savgol_filter(Y, window_length=7, polyorder=3)
+    #X = savgol_filter(X, window_length=7, polyorder=3) # Hasta ahora: 7 y 3
+    #Y = savgol_filter(Y, window_length=7, polyorder=3)
 
     # CALCULO LAS VELOCIDADES USANDO DIFERENCIAS FINITAS DE LAS POSICIONES EN DISTINTOS TIEMPOS
 
-    VX,VY = velocities(X,Y,1/FPS)
+    VX,VY = five_point_stencil(X,Y,1/FPS)
     t = np.arange(0,len(ped))/FPS
-    VX_clean = hampel_filter(VX, window_size=19, n_sigmas=2) # Hasta ahora: 19 y 2 (un desvio estandar por 0.3 segundos)
-    VY_clean = hampel_filter(VY, window_size=19, n_sigmas=2)
-
+    #VX_clean = hampel_filter(VX, window_size=19, n_sigmas=2) # Hasta ahora: 19 y 2 (un desvio estandar por 0.3 segundos)
+    #VY_clean = hampel_filter(VY, window_size=19, n_sigmas=2)
+    VX_clean = hampel_filter(moving_average_smoothing(VX, window_size=5), 19, 2)
+    VY_clean = hampel_filter(moving_average_smoothing(VY, window_size=5), 19, 2)
     # GUARDO LOS DATOS
 
-    data = np.c_[t,X,Y,VX_clean,VY_clean] # Para evitar el smoothing cambiar esto a VX y VY
+    #data = np.c_[t,X,Y,VX,VY]
+    data = np.c_[t,X,Y,VX_clean,VY_clean]   # Para evitar el smoothing cambiar esto a VX y VY
     outFile = 'tXYvXvY' + str(i).zfill(2) + '.txt'
     np.savetxt(outputFolder+outFile, data, delimiter='\t',fmt='%.8e')
   
--- caracterizacion/all.py
+++ caracterizacion/all.py
@@ -4,63 +4,87 @@ import plotly.graph_objects as go
 
 FILES_TO_USE = [i for i in range(0, 14)]
 
+folders = [
+    #"diferenciasFinitas",
+    #"diferenciasFinitasSmoothAndFilter",
+    #"pedestrianTrajectoriesProcessed",
+    "pedestrianTrajectoriesProcessedSmoothAndFilter",
+    #"stencilMovingAverage",
+    "stencilMovingAverageHampel"
+]
+
+colors = [
+    #"red",
+    #"blue",
+    #"green",
+    "orange",
+    #"purple",
+    "green"
+]
+
+names = [
+    #"finite difference",
+    #"fd smooth and filter",
+    #"5 point stencil",
+    "5pt Savitzky-Golay + Hampel",
+    #"5pt moving average",
+    "5pt moving average + Hampel"
+]
+
+figures = {}
 
 keys= []
-time = {}
-vX = {}
-vY = {}
-x = {}
-y = {}
-
 for i in FILES_TO_USE:
     key = f"{i:02}"
     keys.append(key)
-    time[key] = []
-    vX[key] = []
-    vY[key] = []
-    x[key] = []
-    y[key] = []
 
+# Por cada carpeta
+for index, folder_name in enumerate(folders):
+    # Por cada archivo
+    for key in keys:
+        time = []
+        vX = []
+        vY = []
+
+        with open(f"./archivosGerman/{folder_name}/tXYvXvY{key}.txt", "r") as values:
+            lines = values.readlines()
+
+        for line in lines:
+            line_values = line.split(sep='\t')
+            time.append(float(line_values[0]))
+            vX.append(float(line_values[3]))
+            vY.append(float(line_values[4]))
+
+        velocities = []
+
+        for i in range(len(time)):
+            velocities.append(max(abs(vX[i]),abs(vY[i])))
+
+        if key not in figures:
+            figures[key] = go.Figure()   
+        fig = figures[key]
+        fig.add_trace(go.Scatter(x=time, y=velocities, mode='lines', name=names[index], line=dict(color=colors[index]), opacity=1 if index == 0 else 0.5))
+        
+        
 for key in keys:
-    with open(f"./datosCorregidos/tXYvXvY{key}.txt", "r") as values:
-        lines = values.readlines()
+    fig = figures[key]
 
-    for line in lines:
-        line_values = line.split(sep='\t')
-        time[key].append(float(line_values[0]))
-        x[key].append(float(line_values[1]))
-        y[key].append(float(line_values[2]))
-        vX[key].append(float(line_values[3]))
-        vY[key].append(float(line_values[4]))
-
-
-velocities = {}
-for key in keys:
-    curr_vel = []
-    curr_vx = vX[key]
-    curr_vy = vY[key]
-    for i in range(len(time[key])):
-        curr_vel.append(max(abs(curr_vx[i]),abs(curr_vy[i])))
-    velocities[key] = curr_vel
-
-
-for key in keys:    
-    fig = go.Figure()
-
-    fig.add_trace(go.Scatter(x=time[key], y=velocities[key], mode='lines', name=f'v {key}', line=dict(color='blue')))
     fig.update_xaxes(showgrid=False, dtick=5) 
     fig.update_yaxes(showgrid=False)
-        
-
     fig.update_layout(
-        title=f"Velocity vs Time for {key}",
+        title=f"Speed vs Time for {key}",
         xaxis_title="Time (s)",
-        yaxis_title="Velocity (m/s)",
+        yaxis_title="Speed (m/s)",
         template="plotly_white",
-        showlegend=False,
+        showlegend=True,
     )
 
-    fig.show()
-
+    #fig.show()
+    fig.write_image(
+        f"./smoothing_compared/speed_comparison_{key}.png",
+        width=1920,
+        height=1080,
+        scale=4  # Higher scale for better resolution
+    )
 
 
