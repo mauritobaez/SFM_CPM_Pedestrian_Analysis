f500471410d0cecb5ef0287fde87ac49363c4a5e
mauritobaez <mauroleanbaez@gmail.com>
2025-08-25 21:09:36 +0200

    Gran parte desaceleracion

--- analisis/compare.py
+++ analisis/compare.py
@@ -1,86 +1,62 @@
 import json
 import numpy as np
-from scipy.optimize import curve_fit
 import plotly.graph_objects as go
 
-from lib_analisis import acceleration, deceleration
+from lib_analisis import acceleration, best_fit, deceleration, get_events, get_pastos
 
 
 FILES_TO_USE = [i for i in range(1, 15)]  # Use all files from 01 to 14
 folder_name = 'trans_events_by_ped'
+output_file = 'pastos_with_taus'
+output_file_with_no_values = 'taus'
 FPS = 60
+
+def parameters_for_decceleration(i, v, curr_pastos):
+    
+    if i != 0:
+        curr_end = curr_pastos[2*i] - curr_pastos[2*i - 2]
+    v = v[curr_mid: curr_end+1]
+    t = np.arange(len(v)) / FPS
+    return v, t, acceleration, [mid_vel] 
+
+def parameters_for_accleration(i, v, middles):
+    curr_mid = middles[i]
+    mid_vel = v[curr_mid]
+        
+    v = v[start:curr_mid+1]
+    #v = v[curr_mid: curr_end+1]
+    t = np.arange(len(v)) / FPS
+    return v, t, acceleration, [mid_vel]   
 
 keys = []
 for i in FILES_TO_USE:
     key = f"{i:02}"
     keys.append(key)
 
-with open(f"analisis/pastos.json", "r") as f:
-    pastos_data = json.load(f)
-    if 'pastos' in pastos_data:
-        pastos = pastos_data['pastos']
-    else:
-        raise KeyError("Key 'pastos' not found in pastos.json")
+pastos = get_pastos()
 
 for key in keys:
-    events = []
-    for event_number in range(1, 9):
-        t = []
-        x = []
-        y = []
-        v = []
-        with open(f"archivosGerman/{folder_name}/ped_{key}_event_{event_number}.txt", "r") as values:
-            lines = values.readlines()
-        for line in lines:
-            line_values = line.split(sep='\t')
-            t.append(float(line_values[0]))
-            x.append(float(line_values[1]))
-            y.append(float(line_values[2]))
-            v.append(abs(float(line_values[3])))    # Ojo que usamos el valor absoluto de la velocidad
-        events.append({'t': t, 'x': x, 'y': y, 'v': v})
+    events = get_events(folder_name, key)
 
     curr_pastos = pastos[key]['values']
     start = 0 # pastos[key]['start']
-    curr_end = curr_pastos[0] - start
+    #curr_end = curr_pastos[0] - start
     taus = []
     ecms = []
     middles = pastos[key]['middles']
 
     for i, event in enumerate(events):
-        fig = go.Figure()
         v = event['v']
         
-        if i != 0:
-            curr_end = curr_pastos[2*i] - curr_pastos[2*i - 2]
-        
-        curr_mid = middles[i]
-        mid_vel = v[curr_mid]
-        
-        #print(f"Event {i+1} of ped {key} - Start: {start/60}, End: {curr_end/60}, Middle: {curr_mid/60}, Mid Velocity: {mid_vel}")
-        
-        v = v[start:curr_mid+1]
-        #v = v[curr_mid: curr_end+1]
-        t = np.arange(len(v)) / FPS
-        
-        # Fit the exponential function to the velocity data
-        popt, pcov = curve_fit(acceleration(mid_vel), t, v, maxfev=10000)
-
-        tau_fit = popt[0]
-        
-        # Calcular el Error Cuadrático Medio (ECM) entre los valores ajustados y los reales
-        v_fit = acceleration(mid_vel)(t, tau_fit)
-        ecm = np.mean((v - v_fit) ** 2)
-        #print(f"ECM for event {i+1} of ped {key}: {ecm}")
+        t, v, func, func_args = parameters_for_accleration(i, v, middles)
+        tau_fit, ecm = best_fit(t, v, model=func, model_args=func_args)
         ecms.append(ecm)
-
-        #print(f"Best fit parameters for event {i+1} of ped {key}: tau = {tau_fit}")
         taus.append(tau_fit)
         
         if i != len(events) - 1:
             start = curr_pastos[2*i + 1] - curr_pastos[2*i]
     
     pastos[key]['taus'] = taus
-    #print(f"Ped {key} \"taus\": {[float(tau) for tau in taus]}")
     pastos[key]['ecms'] = ecms
     
 
@@ -88,9 +64,10 @@ for key in keys:
 #    pastos[key].pop('values', None)
 #    pastos[key].pop('middles', None)
 #    
-#with open("analisis/taus.json", "w") as f_out:
+#with open("analisis/{output_file_with_no_values}.json", "w") as f_out:
 #    json.dump({'pedestrians': pastos}, f_out, indent=4)
 
-with open("analisis/pastos_with_taus.json", "w") as f_out:
+with open(f"analisis/{output_file}.json", "w") as f_out:
     json.dump({'pastos': pastos}, f_out, indent=4)
     
+ 
--- analisis/lib_analisis.py
+++ analisis/lib_analisis.py
@@ -1,6 +1,8 @@
 
 
+import json
 import numpy as np
+from scipy.optimize import curve_fit
 
 
 # Quizás cambiar el 1.3
@@ -13,3 +15,47 @@ def deceleration(parameter):
     def dece(t, tau):
         return parameter * np.exp(-t / tau)
     return dece
+
+
+def get_pastos():
+    with open(f"analisis/pastos.json", "r") as f:
+        pastos_data = json.load(f)
+        if 'pastos' in pastos_data:
+            pastos = pastos_data['pastos']
+        else:
+            raise KeyError("Key 'pastos' not found in pastos.json")
+    return pastos
+
+
+def get_events(folder_name: str, key: str):
+    events = []
+    for event_number in range(1, 9):
+        t = []
+        x = []
+        y = []
+        v = []
+        with open(f"archivosGerman/{folder_name}/ped_{key}_event_{event_number}.txt", "r") as values:
+            lines = values.readlines()
+        for line in lines:
+            line_values = line.split(sep='\t')
+            t.append(float(line_values[0]))
+            x.append(float(line_values[1]))
+            y.append(float(line_values[2]))
+            v.append(abs(float(line_values[3])))    # Ojo que usamos el valor absoluto de la velocidad
+        events.append({'t': t, 'x': x, 'y': y, 'v': v})
+        
+    return events
+
+
+def best_fit(t, v, model=acceleration, model_args=None):
+    if model_args is None:
+        model_args = []
+    # Fit the exponential modeltion to the velocity data
+    popt, pcov = curve_fit(model(*model_args), t, v, maxfev=10000)
+    tau_fit = popt[0]
+        
+    # Calcular el Error Cuadrático Medio (ECM) entre los valores ajustados y los reales
+    v_fit = model(*model_args)(t, tau_fit)
+    ecm = np.mean((v - v_fit) ** 2)
+    return tau_fit, ecm
+    
